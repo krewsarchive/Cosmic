43598554554a75ada14edda00c30b3ed17fcedfc
Dippy <52082595+Dippys@users.noreply.github.com>
2021-01-12 16:50:40 +0400

    Update Routes For Nitro Intigration

--- Cosmic/Core/Routes.php
+++ Cosmic/Core/Routes.php
@@ -77,7 +77,9 @@ class Routes extends Router
 
                 Router::get('/logout', 'Home\Login@logout');
                 Router::get('/hotel', 'Client\Client@hotel');
+                Router::get('/hotelv2', 'Nitro\Nitro@hotel');
                 Router::get('/client', 'Client\Client@client');
+                Router::get('/nitro', 'Nitro\Nitro@client');
 
                 Router::get('/settings', 'Settings\Preferences@index');
                 Router::get('/settings/email', 'Settings\Email@index');
