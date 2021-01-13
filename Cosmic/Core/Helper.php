bdf0099571deba731a4c6812ee93d40c94d5868d
devraizer <46655802+devraizer@users.noreply.github.com>
2021-01-13 13:15:28 +0100

    Update Helper.php

--- Cosmic/Core/Helper.php
+++ Cosmic/Core/Helper.php
@@ -45,7 +45,7 @@ function response(): Response
 
 function getIpAddress()
 {
-    if(isset(Router::Request()->getHeaders()['http-x-http_x_forwarded_for']) && Router::Request()->getHeaders()['http_x_forwarded_for'] !== NULL) {
+    if(isset(Router::Request()->getHeaders()['http_x_forwarded_for']) && Router::Request()->getHeaders()['http_x_forwarded_for'] !== NULL) {
         return Router::Request()->getHeaders()['http_x_forwarded_for']; 
     }
   
