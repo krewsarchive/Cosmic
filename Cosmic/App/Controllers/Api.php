8a855664c05be2ac6c606da25051cdb72236f5c6
Raizer <raizer@live.nl>
2021-05-27 10:04:05 +0000

    Update Api.php

--- Cosmic/App/Controllers/Api.php
+++ Cosmic/App/Controllers/Api.php
@@ -56,62 +56,6 @@ class Api
         return response()->json($this->callback);
     }
   
-    public function avatars() 
-    {
-        if(empty($_SESSION['auth_ticket'])) {
-            http_response_code(401);
-            return response()->json(['message' => 'no token']);
-        }
-      
-        $player = Player::getDataByAuthToken($_SESSION['auth_ticket']);
-        if(!$player) {
-            http_response_code(401);
-            return response()->json(['message' => 'no token']);
-        }
-      
-        $this->callback = [
-            [
-                "uniqueId" => $_SESSION['auth_ticket'],
-                "name" => $player->username,
-                "figureString" => $player->look,
-                "motto" => $player->motto,
-                "habboClubMember" => true,
-                "lastWebAccess" => "2020-11-19T11:05:03.000+0000",
-                "creationTime" => "2013-12-16T09:57:30.000+0000",
-                "banned" => false
-            ]
-        ];
-      
-        response()->json($this->callback);
-    }
-  
-    public function select()
-    {
-        if(empty($_SESSION['auth_ticket'])) {
-            http_response_code(401);
-            return response()->json(['message' => 'no token']);
-        }
-      
-        $player = Player::getDataByAuthToken($_SESSION['auth_ticket']);
-        if(!$player) {
-            http_response_code(401);
-            return response()->json(['message' => 'no token']);
-        }
-      
-        $this->callback = [
-            "ip" => $player->ip_current,
-            "uniqueId" => $player->auth_ticket,
-            'figureString' => $player->look,
-            "motto" => $player->motto,
-            "buildersClubMember" => false,
-            "habboClubMember" => true,
-            "lastWebAccess" => "2020-11-19T11:05:03.000+0000",
-            "creationTime" => "2013-12-16T09:57:30.000+0000"
-        ];
-      
-        response()->json($this->callback);
-    }
-  
     private function generateSso($player)
     {
         $auth_ticket = Token::authTicket($player->id);
@@ -133,47 +77,6 @@ class Api
         }
     }
   
-    public function login()
-    {
-        $_POST = json_decode(file_get_contents('php://input'), true);
-      
-        $player = Player::getDataByEmail($_POST['email']);
-        if ($player == null || !Hash::verify($_POST['password'], $player->password)) {
-            http_response_code(401);
-            return response()->json(['message' => 'wrong password']);
-        }
-      
-        $_SESSION['auth_ticket'] = $this->generateSso();
-      
-        $this->callback = [
-          [
-          'uniqueId' => $auth_ticket,
-          'name'  => $player->username,
-          'figureString' => $player->look,
-          'motto' => $player->motto,
-          'buildersClubMember' => false,
-          'habboClubMember' => true,
-          'lastWebAccess' => "2020-11-19T11:05:03.000+0000",
-          'creationTime' => "2013-12-16T09:57:30.000+0000",
-          'sessionLogId' => 1337,
-          'loginLogId' => 7331,
-          'email' => $player->mail,
-          'identityId' => 1,
-          'emailVerified' => true,
-          'identityVerified' => true,
-          'identityType' => "HABBO",
-          'trusted' => true,
-          'force' => ["NONE"],
-          'accountId' => $player->id,
-          'country' => "nl",
-          'traits' => ["USER"],
-          'partner' => "NO_PARTNER"
-            ]
-        ];
-
-        response()->json($this->callback);
-    }
-  
     public function vote()
     {
           $FindRetros = new \Library\FindRetros();
@@ -187,14 +90,6 @@ class Api
               ];
           }
           response()->json($this->callback ?? null);
-    }
-  
-    public function noMatch()
-    {
-        response()->json([
-            'error' => 'User Agent does not Match',
-            'user_agent' => request()->getUserAgent()
-        ]);
     }
   
     public function room($roomId)
@@ -246,12 +141,5 @@ class Api
     public function currencys() 
     {
         response()->json(Core::getCurrencys());
-    }
-
-    public static function version()
-    {
-        $version_cosmic = @file_get_contents("https://raw.githubusercontent.com/devraizer/Cosmic/master/Cosmic/public/version.txt");
-        $version = @file_get_contents("version.txt");
-        return ($version_cosmic != $version) ? true : false;
     }
 }
