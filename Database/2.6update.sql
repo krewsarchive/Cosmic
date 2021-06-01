fca3dc88cdabe963875ff6322a9fd01f012c6ad5
Raizer <raizer@live.nl>
2021-06-01 13:07:14 +0000

    Update 2.6 update.sql

---
+++ Database/2.6update.sql
@@ -0,0 +1,3 @@
+INSERT INTO `website_settings` VALUES ('rcon_api_protocol', 'tcp');
+INSERT INTO `website_settings` VALUES ('rcon_api_timeout', '30');
+INSERT INTO `website_settings` VALUES ('rcon_api_persistent', 'false');
