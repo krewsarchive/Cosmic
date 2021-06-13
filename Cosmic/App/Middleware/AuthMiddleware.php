160ba293ea056d085c7ee2c44c01bc5320181d97
Raizer <raizer@live.nl>
2021-06-13 14:14:33 +0000

    Update AuthMiddleware.php

--- Cosmic/App/Middleware/AuthMiddleware.php
+++ Cosmic/App/Middleware/AuthMiddleware.php
@@ -18,21 +18,14 @@ class AuthMiddleware implements IMiddleware
         if(url()->contains('Admin')) {
             $request->setRewriteUrl(url('lost'));            
         }
-<<<<<<< HEAD
-      
+
         $request->player = Player::getDataById(Session::get('player_id'));
         if($request->player == null) {
            return;
-=======
-
-        if(!Session::exists('player_id')) {
-            $request->setRewriteUrl(url('lost'));
-        }
 
         $request->player = Player::getDataById(Session::get('player_id'));
         if($request->player == null) {
-           $request->setRewriteUrl(url('lost'));
->>>>>>> e4604c31e0237059f4f30b8e7297e0c561149dc3
+           return;
         }
       
        if (getIpAddress() != $request->player->ip_current || $_SERVER['HTTP_USER_AGENT'] != Session::get('agent')) {
