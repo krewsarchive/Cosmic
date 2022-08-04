36e8998991108655c4447e565f86531a458131e3
Raizer <raizer@live.nl>
2022-08-04 19:32:00 +0000

    Update ValidationService.php

--- src/System/ValidationService.php
+++ src/System/ValidationService.php
@@ -9,14 +9,13 @@ use Rakit\Validation\Validator;
 
 class ValidationService
 {
-    public static function validate($array, $customMessages = null, $post = null) 
+    public static function validate($array, $post = null) 
     {
         $validator = new Validator();
       
         $validator->addValidator('pattern', new Pattern());
         $validator->addValidator('figure', new Figure());
         $validator->addValidator('captcha', new Captcha());
-        $validator->setMessages($customMessages);
 
         $validation = $validator->validate(($post != null) ? $post : $_POST, $array);
         ($validation->fails()) ? response()->json([
